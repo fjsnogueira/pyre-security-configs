faad3f7460dc5bf0c06993f49038d52bfd40dfad
nwalsh1995 <nwalsh1995@gmail.com>
2019-04-26 02:04:03 -0700

    add os.popen and subprocess.Popen

--- test/source.py
+++ test/source.py
@@ -2,30 +2,42 @@ def os_system(command):
     import os
     os.system(command)
 
+
 def pickle_loads(command):
     import pickle
     pickle.loads(command)
+
 
 def marshal_loads(command):
     import marshal
     marshal.loads(command)
 
+
 def _eval(command):
     eval(command)
+
 
 def subprocess_run(command):
     import subprocess
     subprocess.run(command)
 
-def popen(command):
+
+def subprocess_popen(command):
     import subprocess
     subprocess.Popen(command)
 
+
+def os_popen(command):
+    import os
+    os.popen(command)
+
+
 def convert():
     source = input("user input")
+    subprocess_popen(source)
+    os_popen(source)
     os_system(source)
     pickle_loads(source)
     marshal_loads(source)
     _eval(source)
     subprocess_run(source)
-
